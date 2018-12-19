07b889624811fd6f749ba5ad1394122cac046245
Maria <murikmarusia@gmail.com>
2018-12-19 11:52:31 +0200

    Created addValue function

--- src/js/main.js
+++ src/js/main.js
@@ -1,71 +1,29 @@
-var form = document.querySelector(".form-container");
-var btn = document.querySelector(".btn");
-var fields = document.querySelectorAll(".field");
+(function anon() {
+  const thumbs = document.querySelectorAll('[data-thumb]');
+  const inputs = document.querySelectorAll('[data-input]');
 
-var textVal = document.querySelectorAll(".textVal");
-var emailVal = document.querySelector(".emailVal");
-var passwordVal = document.querySelector(".passwordVal");
-
-
-form.addEventListener("submit", function(event) {
-  event.preventDefault();
-  fields.forEach(element => {
-    element.classList.remove("empty");
-    if (element.value == 0) {
-      element.classList.add("empty");
-    } 
-  });
-  var emptyFieldsCounter = document.querySelectorAll(".empty").length;
-  if (emptyFieldsCounter == 0) {
-    form.submit();
+  const addValue = (inputs, thumbValue, elem) => {
+    inputs.forEach(element => {
+      if(element.dataset.input === elem) {
+        element.setAttribute('value', thumbValue);
+      }
+    });
   }
-});
-
-textVal.forEach(element => {
-  element.addEventListener("input", function(event) {
-    deleteErrors();
-    var abs = element.value;
-    this.style.border = "1px solid green";
-
-    if(abs.match(/['"]/)) {
-      addError(element, "This field can not include &#39 and &#34");
-    }
-  });
-});
-
-emailVal.addEventListener("input", function(event) {
-  deleteErrors();
-  var abs = this.value;
-  this.style.border = "1px solid green";
-
-  if(!(abs.match(/[0-9a-z_]+@[0-9a-z_]+\.[a-z]{2,5}/i))) {
-    addError(emailVal, "This field should be like asd@asd.yu");
-  }
-});
-
-passwordVal.addEventListener("input", function(event) {
-  deleteErrors();
-  var abs = this.value;
-  this.style.border = "1px solid green";
-
-  if(!(abs.match(/[0-9a-z_]{6}/i))) {
-    addError(passwordVal, "At least six characters");
-  }
-});
-
-function addError(element, errorMessage) {
-  var error = document.createElement("div");
-  error.className = "error";
-  error.style.color = "red";
-  error.innerHTML = errorMessage;
-  element.parentElement.appendChild(error);
-  element.style.border = "1px solid red";
-}
-function deleteErrors() {
-  var errors = form.querySelectorAll(".error");
-  for(var i = 0; i < errors.length; i++) {
-    errors[i].remove();
-  }
-}
 
 
+  thumbs.forEach(element => {
+
+    element.addEventListener('mousedown', (e) => {
+      let thumbValue = e.target.value;
+      let elem = element.dataset.thumb;
+
+      addValue(inputs, thumbValue, elem);
+      if(e) {
+        element.addEventListener('mousemove', (e) => {
+          let scrollValue = e.target.value;
+          addValue(inputs, scrollValue, elem);
+        });
+      }
+    });
+  }); 
+}());
